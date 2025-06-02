3649ae4a543ca38c2b5402deaf9cd1a900cdd1d2
Dan <danielsantossouza49@gmail.com>
2025-06-02 11:54:32 -0300

    Create SobreView.axaml.cs

---
+++ Views/SobreView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ProjetoAvalonia.Views;
+
+public partial class SobreView : UserControl
+{
+    public SobreView()
+    {
+        InitializeComponent();
+    }
+}
